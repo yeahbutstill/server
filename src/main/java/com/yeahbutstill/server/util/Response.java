a00d24c80902a492b2a5ebad2921ff227d56a509
Dani Setiawan <mayadani28@gmail.com>
2021-12-17 12:47:50 +0700

    Update util Response class

--- src/main/java/com/yeahbutstill/server/util/Response.java
+++ src/main/java/com/yeahbutstill/server/util/Response.java
@@ -1,9 +1,16 @@
 package com.yeahbutstill.server.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
 
     protected LocalDateTime timeStamp;
@@ -11,5 +18,7 @@ public class Response {
     protected HttpStatus status;
     protected String reason;
     protected String message;
+    protected String devMessage;
+    protected Map<?, ?> data;
 
 }
