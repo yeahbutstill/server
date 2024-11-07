ea126f7f25ba15dacb8c6ec89080f6a7704cf158
yeahbutstill <mayadani28@gmail.com>
2024-11-07 21:49:40 +0700

    update spring-boot 3.3.5

--- src/test/java/com/yeahbutstill/server/ServerApplicationTests.java
+++ src/test/java/com/yeahbutstill/server/ServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.yeahbutstill.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ServerApplicationTests {
 
---
+++ src/test/java/com/yeahbutstill/server/TestServerApplication.java
@@ -0,0 +1,11 @@
+package com.yeahbutstill.server;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(ServerApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
